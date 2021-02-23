5311950df060d7d98c2df6f1e593e9664bf3792f
professordouglasmaioli <76759425+professordouglasmaioli@users.noreply.github.com>
2021-02-23 17:16:32 -0300

    Add files via upload

---
+++ segundo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int inteiro;
+    inteiro = 5;
+    cout << inteiro << endl;
+
+    float real;
+    real = 5.2;
+    cout << real << endl;
+
+    double real2;
+    real2 = 5.2e99;
+    cout << real2 << endl;
+
+    bool booleano;
+    booleano = false;
+    cout << booleano << endl;
+
+    char letra;
+    letra = 'b';
+    cout << letra << endl;
+
+    string palavra;
+    palavra = "bola";
+    cout << palavra << endl;
+
+    int idade;
+    cout << "Qual sua idade?" << endl;
+    cin >> idade;
+    cout << "Idade: " << idade << endl;
+
+
+    return 0;
+}
