29b26bf3eb0e4d3049ffa74287623519d1c44fa5
professordouglasmaioli <76759425+professordouglasmaioli@users.noreply.github.com>
2021-02-27 11:52:26 -0300

    Estruturas de Repetição
    Estruturas de Repetição - While / Do-While / For

---
+++ sexto.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Diga um numero inteiro nao negativo\n";
+    cin >> n;
+
+    int soma=0;
+    int cont=1;
+    //while (cont <= n){
+    //    soma+=cont;
+    //    cont++;
+    //}
+
+    // break
+
+    do{
+        soma+=cont;
+        cont++;
+    } while(cont <= n);
+
+    //for (int j=1 ; j<=n ; j+=1)
+    //    soma+=j;
+    //    cout << "teste\n";
+    
+    
+
+    cout << "Soma dos numeros de 0 ate " << n << "= " << soma << endl;
+
+    return 0;
+}
