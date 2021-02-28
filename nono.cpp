e0db36c2d8258fd7bba1cf4e77fe936ac9827842
professordouglasmaioli <76759425+professordouglasmaioli@users.noreply.github.com>
2021-02-28 20:16:24 -0300

    Matrizes
    Matrizes

---
+++ nono.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int matriz[2][3];
+
+    matriz[0][0] = 11;
+    matriz[0][1] = 12;
+    matriz[0][2] = 13;
+
+    matriz[1][0] = 21;
+    matriz[1][1] = 22;
+    matriz[1][2] = 23;
+
+    for (int i=0 ; i<2 ; i++){
+        for (int j=0 ; j<3 ; j++){
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
